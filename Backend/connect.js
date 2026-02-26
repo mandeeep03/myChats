4afd2ceddd2aad83a4d0391927044df9f5403715
mandeep bhatt <mandeepbhatt52@gmail.com>
2026-02-26 09:20:34 +0530

    creating new java project

--- Backend/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectMongoDB(url){
-    await mongoose.connect(url)
-}
-module.exports = {connectMongoDB}
--- Frontend/src/api.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE = "https://my-chats-1-19gs.onrender.com";
-
